a8373bbb6f1892f25467d0e9ff5abf6870f70996
dennismuturia <dmuturia2922@gmail.com>
2018-02-10 13:27:39 +0300

    Made a few changes

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(function() {
     var formData = $(form).serialize();
 
     //This is to now submit the form using AJAX.
-    $.ajax({type: 'POST' url: $(form).attr('action'), data: formData}).done(function(response) {
+    $.ajax({type: 'POST', url: $(form).attr('action'), data: formData}).done(function(response) {
       // Make sure that the formMessages div has the 'success' class.
       $(formMessages).removeClass('error');
       $(formMessages).addClass('success');
